68ecee5a87750f6918b06fb69c6f5ee427836f50
namiheike <namiheike@gmail.com>
2013-10-13 16:19:37 +0800

    new rails prj

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NewToj::Application.config.secret_key_base = 'cb82636fbb0610dbd0bbb34b1ed997ab411bf49d0c66f6f8ec894b9dfe15c94aaf139446349c2607816008bbaf645b4bff62ab2dd507677e20c7f825d22506f1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NewToj::Application.config.session_store :cookie_store, key: '_new-toj_session'
